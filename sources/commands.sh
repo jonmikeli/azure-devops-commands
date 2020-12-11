bd0239fdedaf39773de3d4f79ac6b0c6d818d72a
Jon Mikel Inza <JonMikel.Inza@CoditMSpartnerFR.onmicrosoft.com>
2020-12-11 18:50:12 +0100

    updates

--- sources/commands.sh
+++ sources/commands.sh
@@ -1,6 +1,10 @@
 #set the environment value AZURE_DEVOPS_EXT_PAT with the PAT value to fully automatize the process
 #If it doesn' work, pipe the variable into the login command
 
+#Variables
+$orgUrl = ""
+$projectName = ""
+
 #1
 az extension add --name azure-devops
 
@@ -9,13 +13,16 @@ az extension add --name azure-devops
 az devops login
 
 #3
-az devops project create --org $orgUrl --name MyNewProject
+az devops project create --org $orgUrl --name $projectName
+
+#3b
+az devops team create --name "" --description "" --ogg $orgUrl -p $projectName
 
 #4-Get the Project Administrators group Id
-az devops security group list -p $projectName --org $orgUrl --query "graphGroups[?contains(principalName,'Project Administrators')].descriptor" -o tsv
+$projAdminGroupDescriptor = az devops security group list -p $projectName --org $orgUrl --query "graphGroups[?contains(principalName,'Project Administrators')].descriptor" -o tsv
 
 #5-Query an org level
-az devops security group list --org $orgUrl --scope organization --query "graphGroups[?contains(principalName,'Specialists')].descriptor" -o tsv
+$specialistGroupDescriptor = az devops security group list --org $orgUrl --scope organization --query "graphGroups[?contains(principalName,'Specialists')].descriptor" -o tsv
 
 #6-Add membership to a Team project group
 az devops security group membership add --org $orgUrl --group-id $projAdminGroupDescriptor --member-id $specialistGroupDescriptor
