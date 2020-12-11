6cc8735ac456275d5ca46c7bbdd7e0b8ee98e3e4
Jon Mikel Inza <JonMikel.Inza@CoditMSpartnerFR.onmicrosoft.com>
2020-12-11 18:37:56 +0100

    pat variable with pipe

--- sources/commands.sh
+++ sources/commands.sh
@@ -1,2 +1,7 @@
 #set the environment value AZURE_DEVOPS_EXT_PAT with the PAT value to fully automatize the process
+#If it doesn' work, pipe the variable into the login command
+
 az extension add --name azure-devops
+
+#$pat | az devops login
+az devops login
