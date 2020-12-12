1af0708f5d3b2a564f530d3ecdc1847c0524256a
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-12-12 12:03:37 +0100

    deleting repository integration into the final script

--- sources/commandsFinal.sh
+++ sources/commandsFinal.sh
@@ -104,6 +104,10 @@ az devops security group membership add --org $orgUrl --group-id $projectRelease
 #8-Create a repo
 az repos create --name $repoName -p "$projectName" --org $orgUrl --verbose
 
+#8b-Delete the default repository
+projectRepositoryId=$(az repos show --repository "$projectName" --org $orgUrl --project "$projectName" --query id -o tsv)
+az repos delete --id $projectRepositoryId --org $orgUrl --project "$projectName" --yes
+
 #9-Add policies
 repositoryId=$(az repos show --repository $repoName --org $orgUrl --project "$projectName" --query id -o tsv)
 az repos policy comment-required create --blocking true --branch master --enabled true --repository-id $repositoryId --org $orgUrl --project "$projectName" --verbose
