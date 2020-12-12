99a758f064e0050e730f8ad2fe857ef22913ea8f
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-12-12 10:10:08 +0100

    new permissions

--- sources/commands2.sh
+++ sources/commands2.sh
@@ -70,7 +70,10 @@ az boards area project create --name "7.4-Assistance" --org $orgUrl --path "\\$p
 #3d
 #Note: the missing 'Area' part in the path is due to the API does not take it into account for boards and team memberships
 az boards area team add --path "\\$projectName\\0-Requirements" --team $customersTeamName --include-sub-areas true --org $orgUrl --project $projectName
+
 az boards area team add --path "\\$projectName\\0-Requirements" --team $managersTeamName --include-sub-areas true --org $orgUrl --project $projectName --set-as-default
+az boards area team add --path "\\$projectName\\5-Tests" --team $managersTeamName --include-sub-areas true --org $orgUrl --project $projectName --set-as-default
+az boards area team add --path "\\$projectName\\7-Others" --team $managersTeamName --include-sub-areas true --org $orgUrl --project $projectName --set-as-default
 
 #3e Delete the default team
 az devops team delete -id "$projectName Team" --org $orgUrl --project $projectName -y
